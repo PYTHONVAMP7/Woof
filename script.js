33832f65446a8a585adffd1755b88847e206fbd4
prasum shrestha <prasumshrestha8877@gmail.com>
2026-01-13 21:44:00 +0545

    woof

--- script.js
+++ script.js
@@ -1,12 +1,77 @@
-is_visible = false
-
-document.querySelector(".fun-btn").addEventListener("click",function visible(){
+let is_visible = false;
+document.querySelector(".fun-btn").addEventListener("click", function visible(e){
+    e.stopPropagation(); 
+    const dropdown = document.querySelector(".dropdown-content");
     if(!is_visible){
-        document.querySelector(".dropdown-content").style.display = "flex";
-        is_visible = true
+        dropdown.style.display = "flex";
+        is_visible = true;
+    } else {
+        dropdown.style.display = "none";
+        is_visible = false;
     }
-    else{
-        document.querySelector(".dropdown-content").style.display = "none";
-        is_visible = false
+});
+
+const namegen = document.querySelector("#namegen")
+
+let woofMode = localStorage.getItem("woofMode") === "true";
+let woofSound = new Audio("woof.mp3");
+woofSound.preload = "auto";
+
+const woofbtn = document.querySelector("#woofstyle");
+
+function updateWoofButton(){
+    if(woofMode){
+        woofbtn.style.backgroundColor = "green";
+        woofbtn.textContent = "Woof Style ON ";
+    } else {
+        woofbtn.style.backgroundColor = "white";
+        woofbtn.textContent = "Woof Style OFF";
     }
-})
+}
+
+updateWoofButton();
+
+woofbtn.addEventListener("click", function(e){
+    e.stopPropagation(); 
+    woofMode = !woofMode;
+    localStorage.setItem("woofMode", woofMode);
+    updateWoofButton();
+});
+
+
+document.addEventListener("click", function(e){
+    if(woofMode && e.target !== woofbtn){
+        woofSound.currentTime = 0;
+        woofSound.play();
+    }
+});
+
+// Dog base names
+const dogNames = [
+    "Buddy", "Max", "Charlie", "Bella", "Luna", "Rocky", "Milo","Coco", "Daisy", "Bailey", "Lucy", "Oliver", "Toby", "Sadie","Zoe", "Jack", "Leo", "Ruby", "Rex", "Maggie", "Nala", "Finn","Oscar", "Lola", "Bear"
+];
+
+const suffixes = [
+    "Bark", "Paws", "Tail", "Woof", "Sniffer", "Fur","Bones", "Howl", "Jump", "Nose", "Whiskers", "Furry","Wag", "Puppy", "Zoom", "Snout", "Fido", "Snuffles","Fluff", "Pupper", "Snouty", "Wiggles", "Pawsy"
+];
+
+
+namegen.addEventListener("click", function() {
+    const dog = dogNames[Math.floor(Math.random() * dogNames.length)];
+    
+    const suf = suffixes[Math.floor(Math.random() * suffixes.length)];
+
+    const generated = `${dog} ${suf}`;
+
+    woofSound.currentTime = 0;
+
+    woofSound.play();
+
+    alert("Your dog name is " + generated)
+});
+
+
+
+
+
+
